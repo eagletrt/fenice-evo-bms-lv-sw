19ca54396b01b3e0671c7fc61b9c200ea2295f55
Dimitri Corraini <dimi56497@yahoo.com>
2024-04-09 11:45:57 +0200

    feat: change to minimun cell voltage value

--- Core/Inc/bms_lv_config.h
+++ Core/Inc/bms_lv_config.h
@@ -17,7 +17,7 @@
 // Number of ntc sensors present in the bms lv
 #define TEMP_SENSOR_COUNT 12
 #define MAX_CELL_VOLTAGE_V (4.2f)
-#define MIN_CELL_VOLTAGE_V (2.8f)
+#define MIN_CELL_VOLTAGE_V (3.0f)
 #define MIN_BATTERY_VOLTAGE_mV ((MIN_CELL_VOLTAGE_V * 1000.0f) * 6.0f)
 #define MAX_CELL_TEMP (60.0f)
 #define MIN_CELL_TEMP (0.0f)
